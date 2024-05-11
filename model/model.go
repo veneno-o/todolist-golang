2598b21874e8f001228e50e0e34a82d20301b818
veneno <1929750203@qq.com>
2024-05-11 22:57:40 +0800

    fix：字符串日期格式处理

--- model/model.go
+++ model/model.go
@@ -1,6 +1,12 @@
 package model
 
 import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -20,4 +26,48 @@ func ConnectDB() {
 	DB.Migrator().CreateTable(&Todo{})
 	DB.Migrator().CreateTable(&User{})
 
+}
+
+type LocalTime time.Time
+
+// 序列化时间格式
+func (t *LocalTime) MarshalJSON() ([]byte, error) {
+	// LocalTime 转换成 time.Time 类型
+	tTime := time.Time(*t)
+	fmt.Println(1)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format("2006-01-02 15:04:05"))), nil
+}
+
+// 反序列化时间处理
+func (t *LocalTime) UnmarshalJSON(data []byte) error {
+	//去除接收的str收尾多余的"
+	timeStr := strings.Trim(string(data), "\"")
+	//转为当前服务器所在时区时间
+	t1, err := time.ParseInLocation("2006-01-02 15:04:05", timeStr, time.Local)
+	// time.Time转换为LocalTime类型
+	*t = LocalTime(t1)
+	if err != nil {
+		return errors.New("时间格式有误，转换失败")
+	}
+	fmt.Println(2)
+
+	return nil
+}
+func (t LocalTime) Value() (driver.Value, error) {
+	fmt.Println(3)
+	var zeroTime time.Time
+	tlt := time.Time(t)
+	//判断给定时间是否和默认零时间的时间戳相同
+	if tlt.UnixMicro() == zeroTime.UnixMicro() {
+		return nil, nil
+	}
+	return tlt, nil
+}
+func (t *LocalTime) Scan(v any) error {
+	if value, ok := v.(time.Time); ok {
+		*t = LocalTime(value)
+		return nil
+	}
+	fmt.Println(4)
+	return fmt.Errorf("不能转换 %V 为时间戳", v)
 }
--- model/todo.go
+++ model/todo.go
@@ -1,16 +1,12 @@
 package model
 
-import (
-	"time"
-)
-
 type Todo struct {
 	ID        uint      `gorm:"primary_key" json:"id"` // 主键ID
-	CreatedAt time.Time `json:"created_at"`            // 创建时间（由GORM自动管理）
-	UpdatedAt time.Time `json:"updated_at"`            // 最后一次更新时间（由GORM自动管理）
+	CreatedAt LocalTime `json:"created_at"`            // 创建时间（由GORM自动管理）
+	UpdatedAt LocalTime `json:"updated_at"`            // 最后一次更新时间（由GORM自动管理）
 	// DeletedAt     gorm.DeletedAt `gorm:"index"`
 	Title         string     `json:"title"`          //任务标题
-	CompletedTime *time.Time `json:"completed_time"` // 完成时间
+	CompletedTime *LocalTime `json:"completed_time"` // 完成时间
 	TaskType      uint       `json:"task_type"`      // 任务类型
 }
 
